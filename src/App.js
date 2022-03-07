af71633d688e99ea540d29255015c6f7696d5023
Shaun Hartman <shaunh@teamsi.com>
2022-03-06 21:13:49 -0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -28,6 +28,12 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+      <div className="bg-white px-4 py-5 border-b border-gray-200 sm:px-6">
+        <h3 className="text-lg leading-6 font-medium text-gray-900">Job Postings</h3>
+        <p className="mt-1 text-sm text-gray-500">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit quam corrupti consectetur.
+        </p>
+      </div>
         <p>
           Show More Posts
         </p>
